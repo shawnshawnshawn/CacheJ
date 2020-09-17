6913b46737e007b90f988623d158ef4a4eeb79da
少年吟风 <858261367@qq.com>
2020-09-17 14:21:12 +0800

    test cache

--- src/main/java/com/aio/cacheJ/CacheApplication.java
+++ src/main/java/com/aio/cacheJ/CacheApplication.java
@@ -1,13 +1,32 @@
 package com.aio.cacheJ;
 
+import com.aio.cacheJ.anno.CacheClear;
+import com.aio.cacheJ.anno.CacheValue;
+import com.aio.cacheJ.intercepter.CacheJSpelExpressionParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CacheApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class, args);
+    }
+
+    @CacheValue(key = "'test:' + #param")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(String param) {
+        return "success";
+    }
+
+    @CacheClear(key = "'test:' + #param")
+    @RequestMapping(value = "/clear", method = RequestMethod.GET)
+    public String clear(String param) {
+        return "success";
     }
 
 }
